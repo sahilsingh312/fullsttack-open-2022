d0819e908ae1a6d645f3c4cf6bdba84c8104fbfa
Sahil Singh <sahil.dogstar@gmail.com>
2022-03-27 22:39:33 +0530

    part 1.3 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 
   const Header = (props) => {
@@ -17,16 +23,16 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
+        <Part part={part1.name} exercises={part1.exercises} />
+        <Part part={part2.name} exercises={part2.exercises} />
+        <Part part={part3.name} exercises={part3.exercises} />
       </div>
     )
   }
 
   const Total = (props) => {
     return (
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     )
   }
 
